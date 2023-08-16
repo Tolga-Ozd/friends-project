2324166d08eb1f56666ded4a2200573ae7ed8e83
Tolga ÖZDİLEK <ozdilektolga7140@gmail.com>
2023-08-16 14:53:00 +0300

    fixed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import {Routes , Route} from "react-router-dom"
-import Main from "./pages/Main"
 
 
 
 function App() {
   return (
     <div className="App">
-     <Main />
+   tolga
 
     </div>
   )
