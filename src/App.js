5d450d953442ae50cb13acdf5aae0e299b12a52f
Tolga ÖZDİLEK <ozdilektolga7140@gmail.com>
2023-08-17 14:49:39 +0300

    task

--- src/App.js
+++ src/App.js
@@ -3,15 +3,39 @@ import CreateTask from "./CreateTask"
 import Header from "./Header"
 
 function App() {
+  const [tasks , setTasks] = useState([]);
 
   const addTask = (newTask) =>{
-      
+    setTasks((prevTask) =>{
+      return {
+        ...prevTask,
+      }
+    })
   }
+
+  const deleteTask =(id) =>{
+    setTasks((prevTask)=>{
+      return prevTask.filter((taskItem , index)=>{
+        return index !=id;
+      })
+    })
+  }
+
 
   return (
     <div className="App">
       <Header />
       <CreateTask  onAdd={addTask}/>
+      {tasks.map((taskItem, index)=>{
+        return(
+          <Task 
+          key={index}
+          id={index}
+          title={taskItem.content}
+          onDelete= {deleteTask}
+           />
+        )
+      })}
     </div>
   )
 }
