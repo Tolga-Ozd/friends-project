899ed3ee1d2f50c0ab895bb49d9b309dcaeb24a9
Tolga ÖZDİLEK <ozdilektolga7140@gmail.com>
2023-08-17 11:33:29 +0300

    createTask

--- src/CreateTask.jsx
+++ src/CreateTask.jsx
@@ -1,20 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
-function CreateTask() {
+function CreateTask(props) {
+  const [task , setTask] = useState({
+    title: "",
+    content : "",
+  })
+
+  function detectChange(e) {
+    const {name , value} = e.target;
+
+    setTask((prevTask) =>{
+      return {
+        ...prevTask,
+        [name] : value,
+      }
+    })
+  }
+
+  function submitTask(e) {
+    props.onAdd(task) ;
+    setTask({
+      title:"",
+      content:"",
+    }),
+    e.preventDefault();
+  }
+
   return (
     <div className='taskdiv'>
         <Form className='taskform'>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+
+            <Form.Group
+             name= "title" 
+             className="mb-3 todoText " 
+             controlId="exampleForm.ControlInput1"
+             value= {task.title}
+             onChange={detectChange}
+             >
+             
                 <Form.Label>Todo Top</Form.Label>
-                <Form.Control type="email" />
+                <Form.Control type="text" />
             </Form.Group>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+
+            <Form.Group 
+            name="content" 
+            className="mb-3  
+            todoText" 
+            controlId="exampleForm.ControlTextarea1"
+            value= {task.content}
+            onChange={detectChange}
+
+            >
                 <Form.Label>Todo textarea</Form.Label>
                 <Form.Control  placeholder="write your to do" as="textarea" rows={3} />
             </Form.Group>
-            <Button size="lg" variant="outline-info">Add</Button>
+
+            <Button size="lg" variant="outline-info" onClick={submitTask}>Add</Button>
 
         </Form>
     </div>
